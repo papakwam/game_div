28136cd62e4e52efaabc20fdb9786d1b15c67081
Owuraku <owurakusintim@gmail.com>
2023-07-03 15:33:23 +0000

    Javascript update

--- vGamePRo.js
+++ vGamePRo.js
@@ -17,8 +17,13 @@ function startGame() {
   document.querySelector(".intro button").addEventListener("click", () => {
     document.querySelector(".intro").style.display = "none";
     document.querySelector(".match").classList.remove("fadeOut");
-  });
+    var sound = new Howl({
+              src: ['./assets/finalsound.mp3']
+               });
 
+               sound.play()
+  
+  });
   // Add click event listeners to option buttons
   const buttons = document.querySelectorAll(".options button");
   buttons.forEach((button) => {
@@ -29,8 +34,29 @@ function startGame() {
 }
 
 function playRound(playerChoice) {
-  let wins = checkWins();
-  if (wins >= 5) {
-    return;
+    let wins = checkWins();
+    if (wins >= 5) {
+      return;
+    }
+  
+    const computerChoice = getComputerChoice();
+    updateHands(playerChoice, computerChoice);
+    const winner = getWinner(playerChoice, computerChoice);
+    updateScore(winner);
+  
+    // Reset the hands after a delay of 2000ms (2 seconds)
+    setTimeout(() => {
+      resetHands();
+    }, 2000);
   }
-} // person 2 should remove this bracket before proceeding...
+  
+  function getComputerChoice() {
+    // Generate a random index to select a choice from the choices array
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+  }
+  
+  
+  // Return the number of wins recorded in the winners array
+  resetGame();
+  startGame();
