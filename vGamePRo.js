d38b60993ec373cedc262413dd7f5112bf0b9c91
Bernard Gyamfi <bernardogyamfi@gmail.com>
2023-07-04 12:46:41 +0000

    checking winner and resetting game

--- vGamePRo.js
+++ vGamePRo.js
@@ -1,4 +1,6 @@
 let winners = []; //Array to store winners
+let playerNumberOfWins = 0; // Current number of wins for the player
+let computerNumberOfWins = 0; //Current number of wins for Computer
 const choices = ["rock", "paper", "scissors"]; //Array of choices
 
 function resetGame() {
@@ -9,6 +11,7 @@ function resetGame() {
   document.querySelector(".player-hand").src = "./assets/rock.png";
   document.querySelector(".computer-hand").src = "./assets/rock.png";
   document.querySelector(".match").classList.add("fadeOut");
+  document.querySelector(".intro").style.display = "flex"; // Display the intro div once again
 }
 
 function startGame() {
@@ -46,10 +49,10 @@ function playRound(playerChoice) {
     (playerChoice === "scissors" && computerChoice === "paper")
   ) {
     document.querySelector(".winner").textContent = "You win!";
-    updateScore(".player-score p");
+    updateScore(".player-score p", "player");
   } else {
     document.querySelector(".winner").textContent = "Computer wins!";
-    updateScore(".computer-score p");
+    updateScore(".computer-score p", "computer");
   }
 }
 
@@ -58,12 +61,36 @@ function getComputerChoice() {
   return choices[randomIndex];
 }
 
-function updateScore(scoreSelector) {
+function updateScore(scoreSelector, competitor) {
   const scoreElement = document.querySelector(scoreSelector);
   let score = parseInt(scoreElement.textContent);
   score++;
+
+  // Check the type of competitor and update its number of wins with the current score
+  if (competitor === "player") {
+    playerNumberOfWins = score;
+  }
+  else {
+    computerNumberOfWins = score;
+  }
   scoreElement.textContent = score.toString();
+
+  console.log("player number of wins is " + playerNumberOfWins);
+  console.log("computer number of wins is " + computerNumberOfWins);
+  if (playerNumberOfWins === 5 || computerNumberOfWins === 5) {
+    //first player to reach 5 wins  is the winner!!
+    if (playerNumberOfWins > computerNumberOfWins) {
+      document.querySelector(".intro > h1").textContent = "YOU WON!!!!";
+    }
+    else {
+      document.querySelector(".intro > h1").textContent = "YOU LOST!!!!";
+    }
+
+
+    // reset game
+    resetGame();
+  }
 }
 
-resetGame();
+
 startGame();
